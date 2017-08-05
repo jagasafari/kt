cda107f3672efc1ae4adc506cf5504ae3c56f123
root <kaszuba.mika@gmail.com>
2017-08-06 01:15:05 +0200

    how to set up command line kotlin compiler on arm7

---
+++ commandLineCompilerARM7.sh
@@ -0,0 +1,12 @@
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | \
+      tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | \
+      tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+apt-get update
+apt-get install oracle-java8-installer
+https://get.sdkman.io > /sdkman.sh
+--edit /sdkman.sh and update sdkman path from /$HOME/.sdkman to /.sdkman
+/sdkman.sh | bash
+--. ~/.bashrc
+sdk install kotlin
