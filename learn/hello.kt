128025324054bc1930a67798ff24d1ef57e20883
root <kaszuba.mika@gmail.com>
2017-08-06 09:02:20 +0200

    var vs val

--- learn/hello.kt
+++ learn/hello.kt
@@ -1,2 +1,16 @@
+class Person {
+    var Name:String = ""
+}
+
 fun main(args: Array<String>){
-    println("hallo world") }
+    println("hallo world") 
+    var mutableInstance = Person()
+    println("mutable Person instance: ${mutableInstance}")
+    mutableInstance = Person()
+    mutableInstance.Name = "next name"
+    println("mutable Person instance mutated: ${mutableInstance}")
+    val immutableInstance = Person()
+    println("imutable instance: ${immutableInstance}")
+    immutableInstance.Name = "assign name"
+    println("imutable instance name var value: ${immutableInstance.Name}")
+}
