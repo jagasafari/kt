aa065535f83cf4ebdd1c6187e7c9a7738cdee0e7
root <kaszuba.mika@gmail.com>
2017-08-07 08:22:43 +0200

    safe null operator for handling null values

--- learn/basicKotlin/basicKotlin.kt
+++ learn/basicKotlin/basicKotlin.kt
@@ -1,18 +1,22 @@
 class Game {
     var Question = "what?"
-    var Answer = ""
+    var Answer:String? = null
+    val AnsNullable:String?=null
 }
 fun main(args: Array<String>){
-    var game = Game()
-    game.Answer = "42"
+    var game:Game? = Game()
+    game?.Answer = null
     var message:String
-    if(game.Answer == "42"){
+    if(game?.Answer == "42"){
         message = "ok"}
     else{
         message="wrong answer"}
     println(message)
-    val msg = if(game.Answer == "42") "ok" else "wrong answer"
+    val msg = if(game?.Answer == "42") "ok" else "wrong answer"
     println(msg)
+    if(game?.AnsNullable != null){
+        println("not null")
+    }
 }
 
 
