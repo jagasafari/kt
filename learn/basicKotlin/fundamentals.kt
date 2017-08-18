4a44b5fbdf50879879a22ac2161939b1564eed2c
root <kaszuba.mika@gmail.com>
2017-08-18 20:42:45 +0200

    static class functions are companion objects

--- learn/basicKotlin/fundamentals.kt
+++ learn/basicKotlin/fundamentals.kt
@@ -1,8 +1,67 @@
+import java.math.BigInteger
+
 fun main(args: Array<String>){
     extenstionFunctionAndRegex()
     infixFunctionAndOperatorOverloading()
+    tailRec()
+    algebraicDataType()
+    singleton()
+    companionObject()
 }
-private fun extenstionFunctionAndRegex(){
+fun companionObject(){
+   MyTimer.staticFun() 
+}
+fun singleton(){
+    println(CacheSingleton.a.size)
+    val filtered = CacheSingleton.a.filter{it>8}.firstOrNull()
+    println(filtered == null)
+}
+object CacheSingleton {
+    var a = arrayListOf<Int>()
+}
+fun algebraicDataType(){
+    val me: MyEvent = MyEvent.Send()
+    when(me){
+        is MyEvent.Send -> println("send event")
+        is MyEvent.Recieve -> println("recieve event")
+    }
+}
+sealed class MyEvent {
+    class Recieve : MyEvent()
+    class Send : MyEvent()
+}
+
+data class MyTime(val h: Int, val m: Int = 0, val s: Int = 0)
+interface Timer {
+    fun setTime(h: Int, m: Int = 0, s: Int = 0)
+    fun setMyTime(t: MyTime) = setTime(t.h)
+}
+interface SameContractInterface {
+    fun setMyTime(t: MyTime) {}
+}
+    
+class MyTimer : Timer, SameContractInterface  {
+    override fun setTime(h: Int, m: Int, s: Int){}
+    override fun setMyTime(t: MyTime): Unit { 
+        super<SameContractInterface>.setMyTime(t)
+    }
+
+    companion object {
+        fun staticFun(){
+            println("calling static function")}
+        fun secondStaticFun(){
+            println("calling second static function")}
+    }
+}
+fun fib(value: Int): BigInteger {
+    tailrec fun fibRec(next: Int, a: BigInteger, b: BigInteger): BigInteger =
+        if (next == 0) b else fibRec(next - 1, a + b, a)
+    return fibRec(value, BigInteger("1"), BigInteger("0"))
+}        
+fun tailRec(){
+    println("fib of 56 is ${fib(56)}")
+}
+fun extenstionFunctionAndRegex(){
     regexOnSpaces("a   // \t ko'")
     "a   // \t ko'".regexOnSpacesExtension()
 }
@@ -11,6 +70,7 @@ operator infix fun Head.plus(head: Head) = this.value + head.value
 private fun infixFunctionAndOperatorOverloading(){
     println(Head(7).plus(Head(7)))
     println(Head(4) plus Head(6))
+    println(Head(8) + Head(0))
 }
 fun regexOnSpaces(str: String)
 {
